bcc55812e76bfed6dbf551f5f42f50252637a27b
Willianson Dantas <williansondantas@gmail.com>
2022-09-23 23:44:31 -0300

    feat: requisito 11, model PostCategory implementado

--- src/models/BlogPost.js
+++ src/models/BlogPost.js
@@ -1,2 +1,56 @@
+const BlogPostModel = (sequelize, DataTypes) => {
+    const BlogPostSchema = sequelize.define("BlogPost", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      content: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      userId: {
+        type: DataTypes.INTEGER,
+      },
+      published: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+      updated: {
+        allowNull: false,
+        type: DataTypes.DATE,
+      },
+    },
+    {
+      timestamps: false, 
+      underscored: true, 
+      tableName: 'blog_posts'
+    });
 
+    BlogPostSchema.associate = (models) => {
+        models.User.belongsTo(models.BlogPost, {
+            as: 'blog_posts',
+            through: BlogPostSchema,
+            foreignKey: 'userId',
+            otherKey: 'id'
+        });
+
+        models.User.belongsTo(models.BlogPost, {
+            as: 'blog_posts',
+            through: BlogPostSchema,
+            foreignKey: 'userId',
+            otherKey: 'id'
+        });
+    }
+  
+    return BlogPostSchema;
+  };
+  
+  module.exports = BlogPostModel;
+  
   
--- src/models/PostCategory.js
+++ src/models/PostCategory.js
@@ -1,10 +1,32 @@
-const UserModel = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-      fullName: DataTypes.STRING,
-      email: DataTypes.STRING,
-    });
-  
-    return User;
-  };
-  
-  module.exports = UserModel;
+const PostCategoryModel = (sequelize, DataTypes) => {
+  const PostCategorySchema = sequelize.define("PostCategory", {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.STRING,
+  },
+  {
+    timestamps: false, 
+    underscored: true, 
+    tableName: 'PostCategories'
+  });
+
+  PostCategorySchema.associate = (models) => {
+      models.BlogPost.belongsToMany(models.Category, {
+          as: 'categories',
+          through: PostCategorySchema,
+          foreignKey: 'categoryId',
+          otherKey: 'postId'
+      });
+
+      models.Category.belongsToMany(models.BlogPost, {
+          as: 'blog_posts',
+          through: PostCategorySchema,
+          foreignKey: 'postId',
+          otherKey: 'categoryId'
+      });
+  }
+
+  return PostCategorySchema;
+};
+
+module.exports = PostCategoryModel;
+
